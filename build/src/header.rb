f7b53bb318936129e5883fa85027d03c44f1aa0a
Rory O’Kane <rory@roryokane.com>
2014-09-04 15:02:23 -0400

    Move `require`s above modules that use them
    In preparation for splitting those modules into separate files and combining them together again in the Makefile

--- build/src/header.rb
+++ build/src/header.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
-require 'openssl'
-require 'stringio'
 require 'pp'
 
 # Hello breakers! Lucky you, I wrote this program in Ruby to save on developer time. Thus, you get to read my source code to look for flaws.
 # Of course, I have accounted for that, and written the program such that that shouldn’t make a difference. I have included no private keys or secret algorithms in here. I invite you to prove me wrong.
 
+require 'openssl'
+require 'stringio'
 module SecureFile
 	KEY_STRETCHING_SALT = "Rory’s custom salt"
 	
@@ -117,6 +117,7 @@ module HTMLExporter
 end
 
 
+require 'stringio'
 class GalleryState
 	def initialize
 		@employee_locations = Hash.new
